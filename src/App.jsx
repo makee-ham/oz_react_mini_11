6c223ebfe7763a40e9c7d21b2968044df6d3ea40
Makee <ina0312@naver.com>
2025-06-17 11:57:15 +0900

    feat: image base url 경로 constants 폴더에 저장

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import { useState } from "react";
 import dummy from "./data/movieListData.json";
 
 function App() {
-  const [movieData, setMovieData] = useState(dummy);
+  const [movieData, setMovieData] = useState(dummy.results);
 
   return (
     <>
       {/* 영화 카드 목록 */}
-      <section></section>
+      <section>{movieData.map()}</section>
     </>
   );
 }
