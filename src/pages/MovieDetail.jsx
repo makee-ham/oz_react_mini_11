42b84f6845d3db9bf62847186cbbf85ee787e183
Makee <ina0312@naver.com>
2025-06-17 13:01:14 +0900

    feat: MovieDetail에 더미 데이터 연결 후 상태로 관리

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import dummy from "../data/movieDetailData.json";
+
 export default function MovieDetail() {
+  const [detailData, _] = useState(dummy);
   return <></>;
 }
