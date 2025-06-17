fe1d07dd32ea964b57a3db9251a2dbd106f7e2b5
Makee <ina0312@naver.com>
2025-06-17 21:11:28 +0900

    chore: chevron 좌우 svg 컴포넌트 추가

---
+++ src/assets/ChevronLeft.jsx
@@ -0,0 +1,19 @@
+export default function ChevronLeft() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="28"
+      viewBox="0 0 16 28"
+      fill="none"
+    >
+      <path
+        d="M14 26L2 14L14 2"
+        stroke="#F1F1F1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ src/assets/ChevronRight.jsx
@@ -0,0 +1,19 @@
+export default function ChevronRight() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="28"
+      viewBox="0 0 16 28"
+      fill="none"
+    >
+      <path
+        d="M2 26L14 14L2 2"
+        stroke="#F1F1F1"
+        stroke-width="4"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
