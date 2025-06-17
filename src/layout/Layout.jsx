631c4dc4a38e54deb25e7b966c6893ac83906fa2
Makee <ina0312@naver.com>
2025-06-17 14:09:50 +0900

    feat: Layout 구조 작성

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,1 +1,13 @@
-export default function Layout() {}
+import { Outlet } from "react-router-dom";
+import NavBar from "./NavBar";
+
+export default function Layout() {
+  return (
+    <>
+      <NavBar />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
