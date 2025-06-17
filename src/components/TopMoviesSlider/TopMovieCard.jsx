293af77eddc54f90cb5ac5b42fae908c5038cab7
Makee <ina0312@naver.com>
2025-06-17 16:02:11 +0900

    feat: top20 영화 카드 컴포넌트 마크업

--- src/components/TopMoviesSlider/TopMovieCard.jsx
+++ src/components/TopMoviesSlider/TopMovieCard.jsx
@@ -1,1 +1,12 @@
-export default function TopMovieCard() {}
+import { TMDB_IMAGE_BASE_URL } from "../../constants/imageBaseUrl";
+
+export default function TopMovieCard({ data, ranking }) {
+  return (
+    <div>
+      <h2>{ranking}</h2>
+      <div>
+        <img src={TMDB_IMAGE_BASE_URL + data.poster_path} alt={data.title} />
+      </div>
+    </div>
+  );
+}
--- src/components/TopMoviesSlider/TopMoviesSlider.jsx
+++ src/components/TopMoviesSlider/TopMoviesSlider.jsx
@@ -2,13 +2,22 @@
 // 마우스 드래그, 터치 스와이프 가능
 // 좌우 Arrows 구현 (호버 시 보이고 아니면 숨겨짐)
 
+import getTopRatedMovies from "../../utils/getTopRatedMovies";
+import movieData from "../../data/movieListData.json";
+import TopMovieCard from "./TopMovieCard";
+
 export default function TopMoviesSlider() {
+  const topMovies = getTopRatedMovies(movieData.results, 20);
   return (
     <section>
       {/* 컨테이너 */}
       <div>
         {/* wrapper */}
-        <div></div>
+        <div>
+          {topMovies.map((movie, idx) => (
+            <TopMovieCard key={movie.id} data={movie} ranking={idx + 1} />
+          ))}
+        </div>
       </div>
       {/* 페이지 인디케이터 */}
       <div></div>
