1b69a7ef99fcb13734091bbf75692fbbe74d9353
Makee <ina0312@naver.com>
2025-06-17 21:38:51 +0900

    design: MovieCard 호버 효과 추가

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,7 +2,7 @@ import { TMDB_IMAGE_BASE_URL } from "../constants/imageBaseUrl";
 
 export default function MovieCard({ poster, title, score }) {
   return (
-    <div className="flex flex-col w-50 h-84 border border-(--line-color) rounded-md overflow-hidden">
+    <div className="flex flex-col w-50 h-84 rounded-lg overflow-hidden bg-(--bg-secondary) hover:shadow-[0_6px_20px_rgba(0,255,255,0.12)] transform transition-all duration-300 hover:-translate-y-1 hover:scale-[1.015]">
       {/* 포스터 이미지 */}
       <div className="aspect-[2/3] w-full overflow-hidden">
         <img
