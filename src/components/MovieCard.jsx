77322b151e6144bf5b3d040f5bb4d8931fc13fca
Makee <ina0312@naver.com>
2025-06-17 12:29:01 +0900

    feat: MovieCard 임시 스타일링

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -2,15 +2,15 @@ import { TMDB_IMAGE_BASE_URL } from "../constants/imageBaseUrl";
 
 export default function MovieCard({ poster, title, score }) {
   return (
-    <div>
+    <div className="flex flex-col w-50 border border-[#ccc]">
       {/* 포스터 이미지 */}
       <div>
         <img src={TMDB_IMAGE_BASE_URL + poster} alt={title} />
       </div>
       {/* 텍스트 영역 */}
-      <div>
-        <h3>{title}</h3>
-        <p>평점: {score}</p>
+      <div className="flex flex-col justify-between h-16 p-2">
+        <h3 className="font-bold">{title}</h3>
+        <p className="font-light text-[#666] text-sm">평점: {score}</p>
       </div>
     </div>
   );
