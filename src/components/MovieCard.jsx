92be743306daedf0ece29dfbce8a0e4456c5a216
Makee <ina0312@naver.com>
2025-06-17 11:32:34 +0900

    feat: MovieCard 기본 마크업 구조 작성

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,3 +1,15 @@
 export default function MovieCard() {
-  return <>MovieCard</>;
+  return (
+    <div>
+      {/* 포스터 이미지 */}
+      <div>
+        <img src={"https://image.tmdb.org/t/p/w500"} alt={"#"} />
+      </div>
+      {/* 텍스트 영역 */}
+      <div>
+        <h3>{"영화 제목"}</h3>
+        <p>{"평점"}</p>
+      </div>
+    </div>
+  );
 }
