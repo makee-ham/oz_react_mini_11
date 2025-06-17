724e5fa0deac9d47921db06981d4c6ba50047e62
Makee <ina0312@naver.com>
2025-06-17 11:48:52 +0900

    feat: App의 상태로 dummy data 관리

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import dummy from "./data/movieListData.json";
 
 function App() {
+  const [movieData, setMovieData] = useState(dummy);
+
   return (
     <>
       {/* 영화 카드 목록 */}
