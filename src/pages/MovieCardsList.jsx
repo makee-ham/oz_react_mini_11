36c12487da0f2a0772dca288a2ec10aa5f274f3f
Makee <ina0312@naver.com>
2025-06-17 13:45:05 +0900

    feat: MovieCard 클릭 시 /detail로 이동하게 Link 설정

--- src/pages/MovieCardsList.jsx
+++ src/pages/MovieCardsList.jsx
@@ -1,18 +1,20 @@
 import { useState } from "react";
 import dummy from "../data/movieListData.json";
 import MovieCard from "../components/MovieCard";
+import { Link } from "react-router-dom";
 
 export default function MovieCardsList() {
   const [movieData, _] = useState(dummy.results);
   return (
     <section className="flex flex-wrap justify-center gap-6 mt-6">
       {movieData.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          poster={movie.poster_path}
-          title={movie.title}
-          score={movie.vote_average}
-        />
+        <Link to="/details" key={movie.id}>
+          <MovieCard
+            poster={movie.poster_path}
+            title={movie.title}
+            score={movie.vote_average}
+          />
+        </Link>
       ))}
     </section>
   );
