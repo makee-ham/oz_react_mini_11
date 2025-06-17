4ba7826b8ff827a9f8f4f373791bc31fc262d5ad
Makee <ina0312@naver.com>
2025-06-17 14:07:28 +0900

    design: font-logo를 Megrim으로 설정

--- src/layout/NavBar.jsx
+++ src/layout/NavBar.jsx
@@ -1,1 +1,14 @@
-export default function NavBar() {}
+export default function NavBar() {
+  return (
+    <header>
+      <h1 className="font-logo">CineVisor</h1>
+      <div>
+        <input type="text" />
+      </div>
+      <div>
+        <button type="button">로그인</button>
+        <button type="button">회원가입</button>
+      </div>
+    </header>
+  );
+}
