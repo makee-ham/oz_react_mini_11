13ec126bd1df796b242248e3fa6a1d6beb0a0bde
Makee <ina0312@naver.com>
2025-06-18 15:31:48 +0900

    feat: TOP RATED MOVIES DATA URL 상수화

--- src/constants/tmdbUrl.js
+++ src/constants/tmdbUrl.js
@@ -1,2 +1,5 @@
 export const POPULAR_MOVIES_DATA_URL =
   "https://api.themoviedb.org/3/movie/popular?language=ko&page=1";
+
+export const TOP_RATED_MOVIES_DATA_URL =
+  "https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1";
