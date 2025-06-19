b6f589e9ffd1c2f4ad13793e92ca7d6173731ccd
Makee <ina0312@naver.com>
2025-06-19 10:02:58 +0900

    feat: 반복되는 api url을 상수로 뺌 Base url

--- src/constants/tmdbUrl.js
+++ src/constants/tmdbUrl.js
@@ -1,5 +1,6 @@
-export const POPULAR_MOVIES_DATA_URL =
-  "https://api.themoviedb.org/3/movie/popular?language=ko&page=1";
+const BASE_URL = "https://api.themoviedb.org/3/movie";
+
+export const POPULAR_MOVIES_DATA_URL = BASE_URL + "/popular?language=ko&page=1";
 
 export const TOP_RATED_MOVIES_DATA_URL =
-  "https://api.themoviedb.org/3/movie/top_rated?language=ko&page=1";
+  BASE_URL + "/top_rated?language=ko&page=1";
