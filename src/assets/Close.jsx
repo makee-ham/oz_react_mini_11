d2f4f5a32a1f792fca45e1502b08e0776f2ed3ad
Makee <ina0312@naver.com>
2025-06-21 21:45:24 +0900

    feat: Close 버튼 컴포넌트 생성

---
+++ src/assets/Close.jsx
@@ -0,0 +1,16 @@
+export default function Close() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 20 20"
+      fill="none"
+    >
+      <path
+        d="M2.53333 19.3333L0.666668 17.4667L8.13333 10L0.666668 2.53334L2.53333 0.666672L10 8.13334L17.4667 0.666672L19.3333 2.53334L11.8667 10L19.3333 17.4667L17.4667 19.3333L10 11.8667L2.53333 19.3333Z"
+        fill="currentColor"
+      />
+    </svg>
+  );
+}
