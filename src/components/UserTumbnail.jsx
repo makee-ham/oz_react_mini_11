a3698ace6a612aae82042990218a99a6a39c98c7
Makee <ina0312@naver.com>
2025-06-22 18:07:56 +0900

    design: thumbnail 사이즈 수정

--- src/components/UserTumbnail.jsx
+++ src/components/UserTumbnail.jsx
@@ -4,7 +4,7 @@ export default function UserThumbnail({ thumbnail = defaultThumb }) {
   return (
     <button
       type="button"
-      className="w-7 h-7 rounded-full mask-radial-at-center mask-radial-from-100% bg-center bg-cover"
+      className="w-8 h-8 rounded-full mask-radial-at-center mask-radial-from-100% bg-center bg-cover"
       style={{ backgroundImage: `url(${thumbnail})` }}
     />
   );
