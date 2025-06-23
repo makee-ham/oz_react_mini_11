5bb65422700dc79beae6642dfbb79910794fb2df
Makee <ina0312@naver.com>
2025-06-23 23:09:56 +0900

    feat: bookmarkAPI 추가

---
+++ src/utils/bookmarkAPI.js
@@ -0,0 +1,37 @@
+import { supabaseClient } from "../supabase/context";
+
+export async function addBookmark(userId, movie) {
+  return await supabaseClient.from("bookmarks").insert({
+    user_id: userId,
+    movie_id: movie.id,
+    title: movie.title,
+    poster_path: movie.poster_path,
+    vote_average: movie.vote_average,
+  });
+}
+
+export async function removeBookmark(userId, movieId) {
+  return await supabaseClient
+    .from("bookmarks")
+    .delete()
+    .match({ user_id: userId, movie_id: movieId });
+}
+
+export async function getBookmarks(userId) {
+  const { data, error } = await supabaseClient
+    .from("bookmarks")
+    .select("*")
+    .eq("user_id", userId);
+
+  return { data, error };
+}
+
+export async function isBookmarked(userId, movieId) {
+  const { data } = await supabaseClient
+    .from("bookmarks")
+    .select("id")
+    .eq("user_id", userId)
+    .eq("movie_id", movieId);
+
+  return data.length > 0;
+}
