af9e3c117f19811271ad9a2da644534d4cca5910
Makee <ina0312@naver.com>
2025-06-29 13:09:53 +0900

    chore: 아이콘 webp화

--- src/components/common/Meta.jsx
+++ src/components/common/Meta.jsx
@@ -4,7 +4,7 @@ export default function Meta({
   title = "CineVisor - 영화, 당신만의 시선으로 보다",
   description = "CineVisor는 최신 영화, 감독, 배우 정보를 한눈에 볼 수 있는 영화 정보 큐레이션 사이트입니다. 마음에 드는 영화를 북마크하고, 감각적인 UI로 영화를 탐색해보세요.",
   keywords = "영화 추천, 최신 영화, 인기 영화, 명작 영화, 영화 장르, TMDB, 영화 배우, 감독, 북마크, 영화 큐레이션, CineVisor, 씨네바이저, 시네바이저",
-  image = "/cvIcon.png",
+  image = "/cvIcon.webp",
   url = "https://oz-react-mini-11-nine.vercel.app/",
   robots = "index, follow",
 }) {
